f1a5d60131274bcaf41255f0e08683c7e830ee0d
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2022-02-24 20:56:27 +0100

    Create image plotting module.

---
+++ src/visualisations/image_plot.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import PIL.Image as pilim
+from matplotlib import pyplot as plt
+from PIL.Image import Image
+from PIL.ImageOps import exif_transpose
+
+from data.dataset.image_pair import ImagePair
+
+
+def resize(image: Image, max_length: int) -> Image:
+    #note: size is (width, height)
+    if image.size[0] > image.size[1]:
+        width = max_length
+        height = image.size[1] * max_length / image.size[0]
+    else:
+        height = max_length
+        width = image.size[0] * max_length / image.size[1]
+
+    return exif_transpose(image.resize((int(width), int(height)), pilim.BILINEAR))
+
+
+def plot_n(image_pairs: list[ImagePair], predicted_similarities: list[bool] = None, fig_size_multiplier: float = 1):
+    assert predicted_similarities is None or len(predicted_similarities) == len(image_pairs)
+
+    plot_images = []
+    plot_titles = []
+
+    for idx in range(len(image_pairs)):
+        pair = image_pairs[idx]
+        image_a = pair.image_a
+        image_b = pair.image_b
+        is_similar = pair.similar
+
+        if predicted_similarities is not None:
+            if predicted_similarities[idx] == is_similar:
+                image_title = f"Ok: Similarity is {is_similar}"
+            else:
+                image_title = f"Wrong: Expected similarity {is_similar}, was {predicted_similarities[idx]}"
+        else:
+            image_title = "Similar" if is_similar else "Different"
+
+        image_a = resize(pilim.open(image_a), int(200 * fig_size_multiplier))
+        image_b = resize(pilim.open(image_b), int(200 * fig_size_multiplier))
+
+        plot_images += [image_a, image_b]
+        plot_titles += [image_title, image_title]
+
+
+    plot(titles=plot_titles, images=plot_images,
+                    rows=len(image_pairs), cols=2, fig_size_multiplier=fig_size_multiplier)
+
+
+def plot(rows: int, cols: int, images: list[Image], titles: list[str], fig_size_multiplier: float = 1):
+    """
+    Plots images with the given titles in a grid.
+    :param rows:
+    :param cols:
+    :param images:
+    :param titles:
+    :param fig_size_multiplier: Multiplier to reduce or increase the size of each image.
+    """
+    fig = plt.figure(figsize=(3 * cols * fig_size_multiplier, 3 * rows * fig_size_multiplier))
+    count = rows * cols
+
+    for idx in range(count):
+        ax = plt.subplot(rows, cols, idx + 1)
+        plt.imshow(images[idx])
+        plt.title(titles[idx])
+        plt.axis("off")
+
+    fig.tight_layout()
+
+
