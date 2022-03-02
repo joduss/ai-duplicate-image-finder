9b205436538941aca456c92aaea768d2d833b75d
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2022-03-02 21:59:26 +0100

    Create notebook with needed side functions to train first model.

---
+++ src/models/euclidian_distance.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+
+def euclidian_distance(vectors):
+    x, y = vectors
+    sum = tf.math.reduce_sum(tf.math.square(x - y), axis=1, keepdims=True)
+    return tf.sqrt(tf.math.maximum(sum, tf.keras.backend.epsilon()))
