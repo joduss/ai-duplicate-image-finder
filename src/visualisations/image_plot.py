96d621e83ba5e63ed0049c127881d077ef195186
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2022-03-05 15:29:58 +0100

    Improve image plot to support custom labels.

--- src/visualisations/image_plot.py
+++ src/visualisations/image_plot.py
@@ -9,7 +9,7 @@ from src.data.dataset.image_pair import ImagePair
 
 
 def resize(image: Image, max_length: int) -> Image:
-    #note: size is (width, height)
+    # note: size is (width, height)
     if image.size[0] > image.size[1]:
         width = max_length
         height = image.size[1] * max_length / image.size[0]
@@ -20,11 +20,15 @@ def resize(image: Image, max_length: int) -> Image:
     return exif_transpose(image.resize((int(width), int(height)), pilim.BILINEAR))
 
 
-def plot_n(image_pairs: list[ImagePair], predicted_similarities: list[bool] = None, fig_size_multiplier: float = 1):
+def plot(image_pairs: list[ImagePair], predicted_similarities: list[bool] = None, fig_size_multiplier: float = 1, titles: list[str] = None):
     assert predicted_similarities is None or len(predicted_similarities) == len(image_pairs)
 
     plot_images = []
     plot_titles = []
+
+    if titles is not None:
+        for title in titles:
+            plot_titles += [title, title]
 
     for idx in range(len(image_pairs)):
         pair = image_pairs[idx]
@@ -32,26 +36,28 @@ def plot_n(image_pairs: list[ImagePair], predicted_similarities: list[bool] = No
         image_b = pair.image_b
         is_similar = pair.similar
 
-        if predicted_similarities is not None:
-            if predicted_similarities[idx] == is_similar:
-                image_title = f"Ok: Similarity is {is_similar}"
+        if titles is None:
+            if predicted_similarities is not None:
+                if predicted_similarities[idx] == is_similar:
+                    image_title = f"Ok: Similarity is {is_similar}"
+                else:
+                    image_title = f"Wrong: Expected similarity {is_similar}, was {predicted_similarities[idx]}"
             else:
-                image_title = f"Wrong: Expected similarity {is_similar}, was {predicted_similarities[idx]}"
-        else:
-            image_title = "Similar" if is_similar else "Different"
+                image_title = "Similar" if is_similar else "Different"
+
+            plot_titles += [image_title, image_title]
 
         image_a = resize(pilim.open(image_a), int(200 * fig_size_multiplier))
         image_b = resize(pilim.open(image_b), int(200 * fig_size_multiplier))
 
         plot_images += [image_a, image_b]
-        plot_titles += [image_title, image_title]
+
+    plot_grid(titles=plot_titles, images=plot_images,
+         rows=len(image_pairs), cols=2, fig_size_multiplier=fig_size_multiplier)
 
 
-    plot(titles=plot_titles, images=plot_images,
-                    rows=len(image_pairs), cols=2, fig_size_multiplier=fig_size_multiplier)
 
-
-def plot(rows: int, cols: int, images: list[Image], titles: list[str], fig_size_multiplier: float = 1):
+def plot_grid(rows: int, cols: int, images: list[Image], titles: list[str], fig_size_multiplier: float = 1):
     """
     Plots images with the given titles in print_loaded_image_name_tf grid.
     :param rows:
@@ -70,5 +76,3 @@ def plot(rows: int, cols: int, images: list[Image], titles: list[str], fig_size_
         plt.axis("off")
 
     fig.tight_layout()
-
-
