d1c6ae4c5055053d45bffa26bd728962fc20a53c
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2022-04-11 07:57:31 +0200

    Restore epsilon to the euclidian_distance computation to prevent NaN errors in the gradient computation.

--- src/math/euclidian_distance.py
+++ src/math/euclidian_distance.py
@@ -4,4 +4,4 @@ import tensorflow as tf
 def euclidian_distance(vectors):
     x, y = vectors
     sum = tf.math.reduce_sum(tf.math.square(x - y), axis=1, keepdims=True)
-    return tf.sqrt(tf.math.maximum(sum, 0))
+    return tf.sqrt(tf.math.maximum(sum, tf.keras.backend.epsilon()))
