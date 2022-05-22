27d4b3fdcd4f034346e12c3633529c8f775a03ac
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2022-05-22 22:32:23 +0200

    Create custom keras layer 'EuclidianDistance'.

--- src/nn/layers/euclidian_distance.py
+++ src/nn/layers/euclidian_distance.py
@@ -1,14 +1,20 @@
 import tensorflow as tf
 
 
-
+@tf.keras.utils.register_keras_serializable(name="EuclidianDistanceLayer")
 class EuclidianDistance(tf.keras.layers.Layer):
 
-    @tf.function
-    def call(self, inputs):
+    def __init__(self, **kwargs):
+        super(EuclidianDistance, self).__init__(**kwargs)
+
+    def call(self, inputs, *args, **kwargs):
 
         x = inputs[0]
         y = inputs[1]
 
         sum = tf.math.reduce_sum(tf.math.square(x - y), axis=1, keepdims=True)
         return tf.sqrt(tf.math.maximum(sum, tf.keras.backend.epsilon()))
+
+
+    def get_config(self):
+        return super(EuclidianDistance, self).get_config()
